72df7d06b8dad9476d92e06a3c0872acccc37252
Matsuoka-source <tm19930425@gmail.com>
2021-07-01 16:30:44 +0900

    Java Study

--- Sample2.java
+++ Sample2.java
@@ -1,6 +1,12 @@
 class Sample2 {
   public static void main(String[] Args) {
+    // mainメソッド開始
     System.out.print("ようこそJavaへ!");
     System.out.print("Javaをはじめましょう!");
+    // printを使うと同じ行に表示される。
+    /*
+     * この記述で囲まれたものは すべてコメントアウトされる
+     */
+    // mainメソッド終了
   }
 }
