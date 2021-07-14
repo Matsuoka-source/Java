6ddb2025c325922ba9d2eca815cac216e9d119c3
Matsuoka-source <tm19930425@gmail.com>
2021-07-15 06:51:50 +0900

    java study

---
+++ java_calculation/Sample2.java
@@ -0,0 +1,15 @@
+class Sample2 {
+  public static void main(String[] args) {
+    int num1 = 2;
+    int num2 = 3;
+    int sum = num1 + num2;
+
+    System.out.println("変数num1は" + num1 + "です。");
+    System.out.println("変数num2は" + num2 + "です。");
+    System.out.println("変数sumは" + sum + "です。");
+
+    num1 = num1 + 1;
+
+    System.out.println("変数num1に１を足すと" + num1 + "です。");
+  }
+}
