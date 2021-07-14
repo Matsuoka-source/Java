c6e2de9afdd6a7215d0924cb86b0d70ea97c578d
Matsuoka-source <tm19930425@gmail.com>
2021-07-14 13:36:53 +0900

    java study

---
+++ Java _variable/Sample3.java
@@ -0,0 +1,16 @@
+class Sample3 {
+  public static void main(String[] args) {
+    int num1, num2;
+
+    num1 = 3;
+
+    System.out.println("変数num1の値は" + num1 + "です。");
+
+    num2 = num1;
+
+    System.out.println("変数num1の値をnum2に代入しました。");
+    System.out.println("変数num1の値は" + num1 + "です。");
+    System.out.println("変数num2の値は" + num2 + "です。");
+  }
+}
+// 他の変数の値を代入する
