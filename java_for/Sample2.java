7c4073c887a147aba5ecf5a1d9bb058f72d76b0b
Matsuoka-source <tm19930425@gmail.com>
2021-07-19 11:18:54 +0900

    java study

---
+++ java_for/Sample2.java
@@ -0,0 +1,8 @@
+class Sample2 {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+      System.out.println(i + "番目の処理が終了しました。");
+    }
+    System.out.println("繰り返しが終わりました。");
+  }
+}
