7cc8884c9b620f85ee257a8e26cc4da8246d0feb
Matsuoka-source <tm19930425@gmail.com>
2021-08-02 17:02:02 +0900

    java study

--- java_class/Sample5.java
+++ java_class/Sample5.java
@@ -1,3 +1,26 @@
+class Car {
+  int num;
+  double gas;
+
+  void setNumGas(int n, double g) {
+    num = n;
+    gas = g;
+    System.out.println("車のナンバーを" + num + "にガソリンを" + gas + "にしました。");
+  }
+
+  void show() {
+    System.out.println("車のナンバーは" + num + "です。");
+    System.out.println("ガソリン量は" + gas + "です。");
+  }
+}
+
 class Sample5 {
-  
+  public static void main(String[] args) {
+    Car car1 = new Car();
+
+    int number = 1234;
+    double gasoline = 20.5;
+
+    car1.setNumGas(number, gasoline);
+  }
 }
