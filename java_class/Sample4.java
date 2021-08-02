fa829829a3b6bb778f17ca518d0670421609c8c7
Matsuoka-source <tm19930425@gmail.com>
2021-08-02 10:08:48 +0900

    java study

--- java_class/Sample4.java
+++ java_class/Sample4.java
@@ -22,7 +22,10 @@ class Sample4 {
   public static void main(String[] args) {
     Car car1 = new Car();
 
-    car1.setNum(1234);
-    car1.setGas(20.5);
+    int number = 1234;
+    double gasoline = 20.5;
+
+    car1.setNum(number);
+    car1.setGas(gasoline);
   }
 }
