e4e8c7b64e3b7e0caf9a749ffa8786ce752fbc82
Matsuoka-source <tm19930425@gmail.com>
2021-08-08 08:00:09 +0900

    java study

--- java_conditions/Sample6.java
+++ java_conditions/Sample6.java
@@ -1,3 +1,29 @@
+// import java.io.*;
+
+// class Sample6 {
+//   public static void main(String[] args) throws IOException {
+//     System.out.println("aかbを入力してください。");
+
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//     String str = br.readLine();
+//     char res = str.charAt(0);
+
+//     switch (res) {
+//       case 'a':
+//         System.out.println("aが入力されました。");
+//         break;
+//       case 'b':
+//         System.out.println("bが入力されました。");
+//         break;
+//       default:
+//         System.out.println("aかbを入力してください。");
+//         break;
+//     }
+//   }
+// }
+// １文字目を認識しているのかander等の言葉でも反応してしまっている。
+
 import java.io.*;
 
 class Sample6 {
@@ -13,13 +39,14 @@ class Sample6 {
       case 'a':
         System.out.println("aが入力されました。");
         break;
+
       case 'b':
         System.out.println("bが入力されました。");
         break;
+
       default:
         System.out.println("aかbを入力してください。");
         break;
     }
   }
 }
-// １文字目を認識しているのかander等の言葉でも反応してしまっている。
--- java_conditions/Sample7.java
+++ java_conditions/Sample7.java
@@ -1,8 +1,34 @@
+// import java.io.*;
+
+// class Sample7 {
+//   public static void main(String[] args) throws IOException {
+//     System.out.println("あなたは男性ですか?");
+//     System.out.println("YまたはNを入力してください。");
+
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//     String str = br.readLine();
+//     char res = str.charAt(0);
+
+//     if (res == 'Y' || res == 'y') {
+//       System.out.println("あなたは男性ですね。");
+//     }
+
+//     else if (res == 'N' || res == 'n') {
+//       System.out.println("あなたは女性ですね。");
+//     }
+
+//     else {
+//       System.out.println("YまたはNを入力してください。");
+//     }
+//   }
+// }
+
 import java.io.*;
 
 class Sample7 {
   public static void main(String[] args) throws IOException {
-    System.out.println("あなたは男性ですか?");
+    System.out.println("あなたは男性ですか？");
     System.out.println("YまたはNを入力してください。");
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,14 +38,11 @@ class Sample7 {
 
     if (res == 'Y' || res == 'y') {
       System.out.println("あなたは男性ですね。");
-    }
-
-    else if (res == 'N' || res == 'n') {
+    } else if (res == 'N' || res == 'n') {
       System.out.println("あなたは女性ですね。");
-    }
-
-    else {
+    } else {
       System.out.println("YまたはNを入力してください。");
     }
+
   }
 }
