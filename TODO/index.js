54128f713f81cb46fcbca568ff749cd1542ad14a
Matsuoka-source <tm19930425@gmail.com>
2021-08-16 16:03:26 +0900

    reload ok

--- TODO/index.js
+++ TODO/index.js
@@ -4,19 +4,49 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if(todos){
+  todos.forEach(todo => {
+    add(todo);
+  })
+}
+
 form.addEventListener("submit",function(event){
   event.preventDefault();
   // console.log(input.value);
   add();
 });
 
-function add(){
+function add(todo){
   let todoText = input.value;
+
+  if(todo){
+    todoText = todo;
+  }
+
   if (todoText){
     const li = document.createElement("li");
     li.innerText = todoText;
     li.classList.add("list-group-item");
     ul.appendChild(li);
     input.value = "";
+    saveData();
   }
 }
+
+function saveData(){
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+  lists.forEach(list=> {
+    todos.push(list.innerText);
+    // console.log(list.innerText);
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+  // console.log(lists);
+
+}
+
+// 暗黙的型変換でif(todoText.length > 0)を省略できる
+
+// ローカルストレージにデータを残すことでリロードしても消えなくなる
