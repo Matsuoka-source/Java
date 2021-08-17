d68a5c68ae8894ad221a0b7b213fe49682430985
Matsuoka-source <tm19930425@gmail.com>
2021-08-17 09:13:53 +0900

    JavaScript_todolist

--- TODO/index.js
+++ TODO/index.js
@@ -1,42 +1,46 @@
 // TODOアプリの開発用
-// console.log('Hello');
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
 const todos = JSON.parse(localStorage.getItem("todos"));
 
-if(todos){
+if(todos) {
   todos.forEach(todo => {
     add(todo);
   })
 }
 
-form.addEventListener("submit",function(event){
+form.addEventListener("submit",function(event) {
   event.preventDefault();
   // console.log(input.value);
   add();
 });
 
-function add(todo){
+function add(todo) {
   let todoText = input.value;
 
-  if(todo){
-    todoText = todo;
+  if(todo) {
+    todoText = todo.text;
   }
 
-  if (todoText){
+  if (todoText) {
     const li = document.createElement("li");
     li.innerText = todoText;
     li.classList.add("list-group-item");
 
-    li.addEventListener("contextmenu", function(event){
+    if(todo && todo.completed) {
+      li.classList.add("text-decoration-line-through");
+    }
+
+    li.addEventListener("contextmenu", function(event) {
       event.preventDefault();
       li.remove();
       saveData();
     });
-    li.addEventListener("click", function(){
+    li.addEventListener("click", function () {
       li.classList.toggle("text-decoration-line-through");
+      saveData();
     });
 
     ul.appendChild(li);
@@ -45,16 +49,20 @@ function add(todo){
   }
 }
 
-function saveData(){
+function saveData() {
   const lists = document.querySelectorAll("li");
   let todos = [];
-  lists.forEach(list=> {
-    todos.push(list.innerText);
+
+  lists.forEach(list => {
+    let todo = {
+      text: list.innerText,
+      completed: list.classList.contains("text-decoration-line-through")
+    };
+    todos.push(todo);
     // console.log(list.innerText);
   });
   localStorage.setItem("todos", JSON.stringify(todos));
   // console.log(lists);
-
 }
 
 // 暗黙的型変換でif(todoText.length > 0)を省略できる
