d9ef49ea9ac9e45e19ebf97f9d0c19431c761e66
Matsuoka-source <tm19930425@gmail.com>
2021-09-03 14:26:01 +0900

    add

--- java_use/Sample8.java
+++ java_use/Sample8.java
@@ -1,10 +1,12 @@
 class Car {
   private int num;
   private double gas;
+  private String name;
 
   public Car() {
     num = 0;
     gas = 0.0;
+    name = "名無し";
     System.out.println("車を作成しました。");
   }
 
@@ -13,40 +15,33 @@ class Car {
     gas = g;
     System.out.println("ナンバーを" + num + "にガソリン量を" + gas + "にしました。");
   }
-  7
 
-  
+  public void setName(String nm) {
+    name = nm;
+    System.out.println("名前を" + name + "にしました。");
+  }
 
   public void show() {
     System.out.println("車のナンバーは" + num + "です。");
     System.out.println("ガソリン量は" + gas + "です。");
+    System.out.println("名前は" + name + "です。");
   }
 }
 
-class Sample7 {
+class Sample8 {
   public static void main(String[] args) {
     Car car1;
-    System.out.println("car1を宣言しました。");
     car1 = new Car();
-    car1.setCar(1234, 20.5);
 
-    Car car2;
-    System.out.println("car2を宣言しました。");
-
-    car2 = car1;
-    System.out.println("car2にcar1を代入しました。");
-
-    System.out.print("car1がさす");
     car1.show();
-    System.out.print("car2がさす");
-    car2.show();
 
-    System.out.println("car1がさす車に変更を加えます。");
-    car1.setCar(2345, 30.5);
+    int number = 1234;
+    double gasoline = 20.5;
+    String str = "1号車";
 
-    System.out.print("car1がさす");
+    car1.setCar(number, gasoline);
+    car1.setName(str);
+
     car1.show();
-    System.out.print("car2がさす");
-    car2.show();
   }
 }
